d962f7d18e508bc301ca1fd0b159c3ef92422cfd
Al-Hussein <alhussein.salam@gmail.com>
2021-07-12 15:58:46 +0300

    added data types and data types limit

---
+++ data_type_sizes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+main()
+{
+
+    printf("%d\n", SCHAR_MIN);
+    printf("%d\n", SCHAR_MAX);
+    printf("%d\n", UCHAR_MAX);
+    printf("%d\n", CHAR_MIN);
+    printf("%d\n", CHAR_MAX);
+    printf("%d\n", SHRT_MIN);
+    printf("%d\n", SHRT_MAX);
+    printf("%d\n", USHRT_MAX);
+    printf("%d\n", INT_MIN);
+    printf("%d\n", INT_MAX);
+    printf("%d\n", UINT_MAX);
+    printf("%d\n", LONG_MIN);
+    printf("%d\n", LONG_MAX);
+    printf("%d\n", ULONG_MAX);
+    printf("%d\n", LLONG_MIN);
+    printf("%d\n", LLONG_MAX);
+    printf("%d\n", ULLONG_MAX);
+    printf("%d\n", CHAR_BIT);
+    printf("%d\n", MB_LEN_MAX);
+
+    printf("%d\n", FLT_MIN);
+    printf("%d\n", FLT_MAX);
+    printf("%d\n", DBL_MIN);
+    printf("%d\n", DBL_MAX);
+    printf("%d\n", LDBL_MIN);
+    printf("%d\n", LDBL_MAX);
+    printf("%d\n", FLT_DIG);
+    printf("%d\n", DBL_DIG);
+    printf("%d\n", LDBL_DIG);
+}
---
+++ data_types.md
@@ -0,0 +1,15 @@
+| Type   |              Size               |  cool |
+| :----- | :-----------------------------: | ----: |
+| char   |             1 byte              | cole3 |
+| int    |        depend on machine        | cole3 |
+| float  | single percision floating-point | cole3 |
+| double | double percision floating-point | cole3 |
+
+
+| Qualifier |       Size       |
+| :-------- | :--------------: |
+| short     |      16 bit      |
+| long      | 16 bit or 32 bit |
+| signed    | 16 bit or 32 bit |
+| unsigned  | 16 bit or 32 bit |
+| long      | 16 bit or 32 bit |
